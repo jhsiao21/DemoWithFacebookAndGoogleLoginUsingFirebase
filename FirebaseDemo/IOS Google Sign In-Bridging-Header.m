863560e67f7b511f19b8c3f09022c8c2085de464
jhsiao21 <jhsiao1121@gmail.com>
2017-05-19 23:34:34 +0800

    initial commit

---
+++ FirebaseDemo/IOS Google Sign In-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  IOS Google Sign In-Bridging-Header.m
+//  FirebaseDemo
+//
+//  Created by Logan on 2017/5/18.
+//  Copyright © 2017年 AppCoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
